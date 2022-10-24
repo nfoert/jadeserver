cd7c2dbb97469bbc200dbe1683c542e6e9f845e1
nfoert <nofoert@gmail.com>
2022-10-24 11:34:31 -0400

    added a footer and a download page

--- jadeserver/jadesite/views.py
+++ jadeserver/jadesite/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from jadeLauncher.models import Version
 
 def index(request):
     url = request.get_full_path()
@@ -9,4 +10,11 @@ def index(request):
     return render(request, "index.html", context)
 
 def download(request):
-    return render(request, "download.html")
+    versionData = Version.objects.all()
+    major = versionData[0].major
+    minor = versionData[0].minor
+    patch = versionData[0].patch
+    context = {
+        "version": f"{major}.{minor}.{patch}",
+    }
+    return render(request, "download.html", context)
